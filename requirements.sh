5b4728ca6eb3fbfcbe5bde61175d450974451490
Prasham Patel <91484142+Prasham-Patel@users.noreply.github.com>
2024-04-14 11:27:21 -0400

    Delete requirements.sh

--- requirements.sh
+++
@@ -1,8 +0,0 @@
-sudo pip install numpy
-sudo apt-get install swig
-sudo pip install box2d-py
-sudo pip install gym
-sudo pip install tensorflow
-pip install tensorflow[and-cuda]
-sudo pip install matplotlib
-sudo pip install tf-keras
